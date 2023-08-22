a4a4ff7a390c40edc5462d37bf510b2ece0c5a7e
Cloud User <ec2-user@ip-172-31-15-252.us-east-2.compute.internal>
2023-08-22 06:06:15 +0000

    Functioning code tf + kub + python + dockerfile

--- README.md
+++ README.md
@@ -1,19 +1,31 @@
-Configure aws cli to kubectl (note)
-    aws eks update-kubeconfig --region us-east-2 --name weather-microservice
-    kubectl get nodes
-
-Installs:
-    curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
-    curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
-    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-
-Resources: 
-    https://registry.terraform.io/modules/terraform-aws-modules/eks/aws/latest
-    https://api.open-meteo.com/v1/forecast?latitude=38.8951&longitude=-77.0364&daily=temperature_2m_max,temperature_2m_min,precipitation_sum&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=GMT'
-    https://www.fullstackpython.com/flask-json-jsonify-examples.html
+- Instructions: 
+    *  aws eks update-kubeconfig --region us-east-2 --name weather-microservice
+    * kubectl create namespace jenkins
+    * kubectl apply -f jenkins-deployment.yml -n jenkins
+    * kubectl apply -f jenkins-lb.yml -n jenkins
+    * kubectl get svc jenkins -n jenkins
+    * POD_NAME=$(kubectl get pods -l app=jenkins -n jenkins -o jsonpath="{.items[0].metadata.name}")
+    * kubectl logs $POD_NAME -n jenkins | grep -A 5 "Jenkins initial setup is required"
 
 
-DONT REINVENT THE WHEEL     
-Helm chart jenkins
-    https://charts.jenkins.io/
-    https://helm.sh/docs/
+
+- Installs:
+    - curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+    - curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+    - curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+
+- Resources: 
+    - https://registry.terraform.io/modules/terraform-aws-modules/eks/aws/latest
+    - https://api.open-meteo.com/v1/forecast?latitude=38.8951&longitude=-77.0364&daily=temperature_2m_max,temperature_2m_min,precipitation_sum&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=GMT'
+    - https://www.fullstackpython.com/flask-json-jsonify-examples.html
+    - https://registry.terraform.io/providers/hashicorp/kubernetes/latest/docs/data-sources/storage_class
+    - https://registry.terraform.io/providers/hashicorp/kubernetes/latest/docs
+    - https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/eks_cluster
+
+
+    - https://helm.sh/docs/
+
+
+- weather-app/source/ref/output_rf
+    - Output to get understanding what the set up is doing
+    - grabbed eks.node_secuirty_id so it could be set dynmaiclly vs statically
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,3 @@
-# AWS provider for east 2 region 
 provider "aws" {
   region = "us-east-2"
 }
@@ -11,42 +10,80 @@ terraform {
   }
 }
 
-# EKS module 
-module "eks" {
-  source          = "terraform-aws-modules/eks/aws"  
-  cluster_name    = "weather-microservice" 
-  version         = "19.16.0" 
-  vpc_id          = "vpc-00a8c79fe20baab1f"                  
-  cluster_version = "1.27"                         
-  subnet_ids      = ["subnet-0a426585c2fdad736", "subnet-060391912e702e8e1"] 
+resource "aws_security_group" "eks_security_group" {
+  name        = "eks_security_group"
+  description = "Security Group for EKS Cluster"
+  vpc_id      = "vpc-00a8c79fe20baab1f"
+}
 
-  # configure 2 nodes to keep cost down. 
+resource "aws_security_group_rule" "jenkins_inbound_http" {
+  type              = "ingress"
+  from_port         = 8080
+  to_port           = 8080
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.eks_security_group.id
+}
+
+resource "aws_security_group_rule" "jenkins_inbound_jnlp" {
+  type              = "ingress"
+  from_port         = 50000
+  to_port           = 50000
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.eks_security_group.id
+}
+
+resource "aws_security_group_rule" "allow_all_outbound" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.eks_security_group.id
+}
+
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = "weather-microservice"
+  version         = "19.16.0"
+  vpc_id          = "vpc-00a8c79fe20baab1f"
+  cluster_version = "1.27"
+  subnet_ids      = ["subnet-0a426585c2fdad736", "subnet-060391912e702e8e1"]
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = true
+  iam_role_arn = "arn:aws:iam::575441150451:role/full_eks"
+
   eks_managed_node_groups = {
     eks_nodes = {
       desired_capacity = 1
       max_capacity     = 2
       min_capacity     = 1
-      instance_type    = "t2.micro"
+      instance_type    = "t2.medium"
+      iam_role_arn     = "arn:aws:iam::575441150451:role/full_eks"
+      create_iam_role  = false
+      additional_security_group_ids = [aws_security_group.eks_security_group.id]
     }
   }
 }
 
+#ADD FIX BELOW TO OPEN EVERYTHIING AFTER THE ABOVE CODE IS RAN
 # add inbound rule to allow all traffic
-resource "aws_security_group_rule" "allow_all_inbound" {
+resource "aws_security_group_rule" "allow_all_inbound_fix" {
   type        = "ingress"
   from_port   = 0
   to_port     = 0
   protocol    = "-1"  
   cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = "sg-0ac7be7b2220afa62"
+  security_group_id = "sg-0ced0512efd3633ea" # Replace with Additional security groups for EKS cluster 
 }
 
 # add outbound rule to allow all traffic
-resource "aws_security_group_rule" "allow_all_outbound" {
+resource "aws_security_group_rule" "allow_all_outbound_fix" {
   type        = "egress"
   from_port   = 0
   to_port     = 0
   protocol    = "-1"  
   cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = "sg-0ac7be7b2220afa62"
+  security_group_id = "sg-0ced0512efd3633ea" # Replace with Additional security groups for EKS cluster 
 }
