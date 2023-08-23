2687af56fe9bc7872ae92735e3a241ae9a21533d
surferdudeken <86377600+surferdudeken@users.noreply.github.com>
2023-08-22 22:43:45 -0700

    Update initial_setup.sh

--- initial_setup.sh
+++ initial_setup.sh
@@ -9,17 +9,21 @@ kubectl create namespace jenkins
 kubectl apply -f ../kub/jenkins-deployment.yml -n jenkins
 kubectl apply -f ../kub/jenkins-lb.yml -n jenkins
 
-# cd ../source/
-# podman build -t weather-app:latest .
+
 clear 
 
 echo "Waiting for Jenkins to be accessible..."
 sleep 180 
-JENKINS_IP=$(kubectl get svc jenkins -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-echo "Jenkins should be accessible at: http://${JENKINS_IP}:8080"
-
+JENKINS_IP=$(kubectl get svc jenkins -n jenkins -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')
 POD_NAME=$(kubectl get pods -l app=jenkins -n jenkins -o jsonpath="{.items[0].metadata.name}")
 PASSWORD=$(kubectl logs $POD_NAME -n jenkins | grep -A 5 "Jenkins initial setup is required")
 
+echo "###########################################################"
+echo "###########################################################"
+echo "Jenkins should be accessible at: http://${JENKINS_IP}:8080"
+echo "###########################################################"
+echo "###########################################################"
 echo $PASSWORD
-echo "Please configure changes as per the README.md file" 
+echo "###########################################################"
+echo "###########################################################"
+echo "Please configure changes to jenkins as per the README.md file" 
